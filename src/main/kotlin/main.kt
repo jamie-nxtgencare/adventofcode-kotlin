a59d76b5561fdb01dfa68f87f2fa56ebf70ad3a5
jamie <jamie@checkin.tech>
2020-12-02 16:22:38 -0400

    #5: Parameterizing main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,23 @@
 import java.io.File
 
-fun main() {
-/*    val sample = DayOne("day1.sample-input")
-    println(sample.part1())
-    println(sample.part2())
+fun main(args: Array<String>) {
+    if (args[0] == "1") {
+        val sample = DayOne("day1.sample-input")
+        println(sample.part1())
+        println(sample.part2())
 
-    val input = DayOne("day1.input")
-    println(input.part1())
-    println(input.part2())*/
+        val input = DayOne("day1.input")
+        println(input.part1())
+        println(input.part2())
+    } else if (args[0] == "2") {
+        val sample = DayTwo("day2.sample-input")
+        println(sample.part1())
+        println(sample.part2())
 
-    val sample = DayTwo("day2.sample-input")
-    println(sample.part1())
-    println(sample.part2())
-
-    val input = DayTwo("day2.input")
-    println(input.part1())
-    println(input.part2())
+        val input = DayTwo("day2.input")
+        println(input.part1())
+        println(input.part2())
+    }
 }
 
 fun getLines(file: String) : List<String> {
