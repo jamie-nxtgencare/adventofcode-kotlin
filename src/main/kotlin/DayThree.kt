9fd343ff3300fc5f318b4568d1769b1f4628bf97
jamie <jamie@checkin.tech>
2020-12-04 10:58:52 -0400

    Cleanup

--- src/main/kotlin/DayThree.kt
+++ src/main/kotlin/DayThree.kt
@@ -9,10 +9,9 @@ class DayThree(file: String) : Project {
         return traverse(3)
     }
 
-    private fun traverse(xSlope: Int) = traverse(xSlope, 1)
-
-    private fun traverse(xSlope: Int, ySlope: Int): Int {
+    private fun traverse(xSlope: Int, ySlope: Int = 1): Int {
         var out = 0
+
         for (i in 0 until grid.size / ySlope) {
             val row = grid[i * ySlope]
             if (row[i * xSlope % row.size]) {
