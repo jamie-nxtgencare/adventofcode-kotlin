40e36b764f534eeeac762e4e43dbbea291e00ee5
jamie <jamie@checkin.tech>
2020-12-15 17:02:46 -0400

    #34: Performance

--- src/main/kotlin/DayFifteen.kt
+++ src/main/kotlin/DayFifteen.kt
@@ -1,37 +1,25 @@
 class DayFifteen(file: String): Project {
-    val debug = false
-    val input = getLines(file)[0].split(",").map { it.toInt() }
+    private val input = getLines(file)[0].split(",").map { it.toInt() }
 
     override fun part1(): Any {
         return speak(2020)
     }
 
     override fun part2(): Any {
-        return speak(30000000)
+        return speak(30_000_000)
     }
 
     private fun speak(number: Int): Int {
-        val lastSeenMap = HashMap<Int, Int>()
+        val lastSeenMap = arrayOfNulls<Int>(30_000_000)
 
         for (i in 0..input.size-2) { lastSeenMap[input[i]] = i }
         var lastNum = input[input.size - 1]
 
         var i = input.size - 1
-        var j = i + 2
-        while (j <= number) { /* +3 = 1 for 0 index, 1 for reading "last spoken" each i, 1 for "until" */
+        while (i < number - 1) {
             val oldLast = lastNum
-            lastNum = when (val maybeLastNum = lastSeenMap[lastNum]) {
-                null -> 0
-                else -> i - maybeLastNum
-            }
-            lastSeenMap[oldLast] = i
-
-            if (debug) {
-                println("$j: $lastNum")
-            }
-
-            i++
-            j++
+            lastNum = lastSeenMap[lastNum]?.let { i - it } ?: 0
+            lastSeenMap[oldLast] = i++
         }
 
         return lastNum
