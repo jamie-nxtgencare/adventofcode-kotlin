b4e470a802b5a9d167ff02d6966f345c8d10d249
jamie <jamie@checkin.tech>
2021-12-01 09:31:21 -0400

    #2: Default base for hub pull request command is master, creation defaults to main now.

--- scripts/issue_pr.sh
+++ scripts/issue_pr.sh
@@ -4,4 +4,4 @@ branch=`git status | head -n 1 | awk '{ print $3; }'`
 issue=`echo $branch | awk -F / '{ print $2; }'`
 echo 'Issuing PR for current branch:' $branch 'to issue' $issue;
 
-hub pull-request -i $issue -h jamie-nxtgencare:$branch
+hub pull-request -i $issue -b "jamie-nxtgencare:main" -h "jamie-nxtgencare:$branch"
