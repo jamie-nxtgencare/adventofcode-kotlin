f30e05fbd0b51a8d5b8d1c583db3cca177a5e4ca
jamie <jamie@checkin.tech>
2021-12-02 16:24:45 -0400

    #4: day 2 part 1

--- src/main/kotlin/DayTwo.kt
+++ src/main/kotlin/DayTwo.kt
@@ -1,10 +1,30 @@
 class DayTwo(file: String) : Project {
+    private val instructions = mapFileLines(file) { Instruction(it) }
+
     override fun part1(): Any {
-       return -1
+       var output = Pair(0, 0)
+        instructions.forEach { output = it.apply(output) }
+        return output.first * output.second
     }
 
     override fun part2(): Any {
         return -1
     }
 
+}
+
+class Instruction(it: String) {
+    private val instructionStr = it.split(" ")
+    private val instruction = instructionStr[0]
+    private val amount: Int = instructionStr[1].toInt()
+
+    fun apply(coord: Pair<Int, Int>): Pair<Int, Int> {
+        return when(instruction) {
+            "forward" -> Pair(coord.first.plus(amount), coord.second)
+            "backward" -> Pair(coord.first.minus(amount), coord.second)
+            "up" -> Pair(coord.first, coord.second.minus(amount))
+            "down" -> Pair(coord.first, coord.second.plus(amount))
+            else -> Pair(coord.first, coord.second)
+        }
+    }
 }
