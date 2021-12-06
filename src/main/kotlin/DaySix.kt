51fb56b40af4eeb2f2f848c4c474e034edaf9165
jamie <jamie@checkin.tech>
2021-12-06 09:45:39 -0400

    #8: Day 6 part 2

--- src/main/kotlin/DaySix.kt
+++ src/main/kotlin/DaySix.kt
@@ -1,13 +1,24 @@
+
 class DaySix(file: String) : Project {
     private val input: List<Int> = getLines(file).first().split(",").map { it.toInt() }
-    private val ageCounts = Array(9) { 0 }
+    private val ageCounts: Array<Long> = Array(9) { 0L }
+    private val initialAgeCounts: Array<Long> = Array(9) { 0L }
 
     init {
         input.forEach { ageCounts[it]++ }
+        ageCounts.copyInto(initialAgeCounts)
     }
 
     override fun part1(): Any {
-        val days = 80
+        return getAnswer(80)
+    }
+
+    override fun part2(): Any {
+        initialAgeCounts.copyInto(ageCounts)
+        return getAnswer(256)
+    }
+
+    private fun getAnswer(days: Int): Long {
         for (i in days - 1 downTo 0) {
             advanceDay()
         }
@@ -15,12 +26,8 @@ class DaySix(file: String) : Project {
         return ageCounts.sum()
     }
 
-    override fun part2(): Any {
-        return -1
-    }
-
     fun advanceDay() {
-        var tmp = 0
+        var tmp = 0L
         for (i in ageCounts.size downTo 0) {
             if (i > 0) {
                 val tmp2 = ageCounts[i - 1]
