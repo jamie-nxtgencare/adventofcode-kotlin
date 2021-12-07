8f2001aa41c536a5b43301891580bdf13c80576c
jamie <jamie@checkin.tech>
2021-12-07 12:30:35 -0400

    #9: Day 7 part 1

--- src/main/kotlin/DaySeven.kt
+++ src/main/kotlin/DaySeven.kt
@@ -1,6 +1,21 @@
+import kotlin.math.abs
+
 class DaySeven(file: String) : Project {
+    private val subs = getLines(file).first().split(",").map { it.toInt() }
+    private val uniqueSubs = subs.distinct()
+    private val sortedUnique = uniqueSubs.sorted()
+
     override fun part1(): Any {
-        return -1
+        val start = sortedUnique.first()
+        val end = sortedUnique.last()
+
+        val distances: ArrayList<Int> = ArrayList()
+
+        for (i in start..end) {
+            distances.add(subs.sumOf { abs(it - i) })
+        }
+
+        return distances.minOrNull() ?: -1
     }
 
     override fun part2(): Any {
--- src/test/kotlin/Testing.kt
+++ src/test/kotlin/Testing.kt
@@ -65,7 +65,7 @@ class Testing {
 
     @Test
     fun day7Sample() {
-        testSample(7, -1, -1)
+        testSample(7, 37, -1)
     }
 
     @Test
