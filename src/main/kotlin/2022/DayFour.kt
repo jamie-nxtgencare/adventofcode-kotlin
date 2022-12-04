a5a37a391626d4fd3cb4fc13c2c2b3189f46ebaa
jamie <jamie@checkin.tech>
2022-12-04 08:03:05 -0400

    #8: 2022, Day 4, Part 1

--- src/main/kotlin/2022/DayFour.kt
+++ src/main/kotlin/2022/DayFour.kt
@@ -5,8 +5,24 @@ package `2022`
 import Project
 
 class DayFour(file: String) : Project {
+    private val sections = mapFileLines(file) { Section(it) }
+    // 2-4,6-8
+
+    class Section(line: String) {
+        private val range1 = getRange(line.split(",")[0])
+        private val range2 = getRange(line.split(",")[1])
+
+        private fun getRange(s: String): IntRange {
+            return IntRange(s.split("-")[0].toInt(), s.split("-")[1].toInt())
+        }
+
+        fun fullyOverlaps(): Boolean {
+            return (range1.first >= range2.first && range1.last <= range2.last) || (range2.first >= range1.first && range2.last <= range1.last)
+        }
+    }
+
     override fun part1(): Any {
-        return -1
+        return sections.filter { it.fullyOverlaps() }.count()
     }
 
     override fun part2(): Any {
