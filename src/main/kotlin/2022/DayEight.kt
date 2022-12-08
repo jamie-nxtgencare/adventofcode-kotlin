affb1f6619ae5ee64d3617dbe04d3a874276f96d
jamie <jamie@checkin.tech>
2022-12-08 11:54:51 -0400

    #13: 2022, Day 8, Part 2

--- src/main/kotlin/2022/DayEight.kt
+++ src/main/kotlin/2022/DayEight.kt
@@ -7,20 +7,6 @@ import Project
 class DayEight(file: String) : Project {
     val grid = mapLettersPerLines(file) { it.map { it2 -> it2.toString().toInt() }.toList() }.toList()
 
-    override fun part1(): Any {
-        var visible = 0
-        for (y in grid.indices) {
-            for (x in grid[y].indices) {
-                if (isVisible(x, y)) {
-                    visible++
-                }
-            }
-        }
-
-        printGrid(grid)
-        return visible
-    }
-
     private fun isVisible(x: Int, y: Int): Boolean {
         if (x == 0 || y == 0 || x == grid[y].size - 1 || y == grid.size - 1) {
             return true
@@ -32,6 +18,15 @@ class DayEight(file: String) : Project {
         val bottomVisible = checkCol(y + 1, grid.size, x, y)
 
         return leftVisible || rightVisible || topVisible || bottomVisible
+    }
+
+    private fun getScenicScore(x: Int, y: Int): Int {
+        val upScore = checkCol2(y - 1 downTo 0, x, y)
+        val leftScore = checkRow2(x - 1 downTo 0, x, y)
+        val downScore = checkCol2(y + 1 until grid.size, x, y)
+        val rightScore = checkRow2(x + 1 until grid[y].size, x, y)
+
+        return upScore * leftScore * downScore * rightScore
     }
 
     private fun checkRow(start: Int, end: Int, x:Int, y: Int): Boolean {
@@ -47,6 +42,21 @@ class DayEight(file: String) : Project {
         return visible
     }
 
+    private fun checkRow2(range: IntProgression, x:Int, y: Int): Int {
+        var visible = true
+        var score = 0
+        for (i in range) {
+            if (!visible) {
+                break
+            }
+
+            visible = grid[y][i] < grid[y][x]
+            score++
+        }
+
+        return score
+    }
+
     private fun checkCol(start: Int, end: Int, x:Int, y: Int): Boolean {
         var visible = true
         for (i in start until end) {
@@ -60,17 +70,43 @@ class DayEight(file: String) : Project {
         return visible
     }
 
-    override fun part2(): Any {
-        return -1
+    private fun checkCol2(range: IntProgression, x:Int, y: Int): Int {
+        var visible = true
+        var score = 0
+        for (i in range) {
+            if (!visible) {
+                break
+            }
+
+            visible = grid[i][x] < grid[y][x]
+            score++
+        }
+
+        return score
     }
 
-    fun printGrid(grid: List<List<Int>>) {
-        grid.forEachIndexed { y, it ->
-            it.forEachIndexed { x, o ->
-                print(if (isVisible(x, y)) "#" else ".")
+    override fun part1(): Any {
+        var visible = 0
+        for (y in grid.indices) {
+            for (x in grid[y].indices) {
+                if (isVisible(x, y)) {
+                    visible++
+                }
             }
-            println()
         }
-        println()
+
+        return visible
+    }
+
+    override fun part2(): Any {
+        val scores: ArrayList<Int> = ArrayList()
+
+        for (y in grid.indices) {
+            for (x in grid[y].indices) {
+                scores.add(getScenicScore(x, y))
+            }
+        }
+
+        return scores.max()
     }
 }
