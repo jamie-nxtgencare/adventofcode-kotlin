c976d9ebae4649f4cd86483d7e888699adfbeb99
jamie <jamie@checkin.tech>
2022-12-14 12:55:15 -0400

    Returning to my nemesis

--- src/main/kotlin/2019/DayFourteen.kt
+++ src/main/kotlin/2019/DayFourteen.kt
@@ -13,27 +13,23 @@ class DayFourteen(file: String) : Project {
             val inputAndOutput = it.split(" => ")
             val inputs = inputAndOutput[0].split(",").map { it2 ->
                 val inputStr = it2.trim().split(" ")
-                CountAndType(inputStr[0].toInt(), inputStr[1])
+                CountAndType(inputStr[0].toLong(), inputStr[1])
             }
 
             val output = inputAndOutput[1].trim().split(" ")
 
-            val reaction = Reaction(inputs, CountAndType(output[0].toInt(), output[1]))
+            val reaction = Reaction(inputs, CountAndType(output[0].toLong(), output[1]))
             reaction
         }.groupBy{ it.output.type }.mapValues { it.value.first() }
     }
 
-    override fun part1(): Any {
-        val fuel = reactions["FUEL"]!!
-        val required = ArrayList<CountAndType>()
-        val extras = HashMap<String, Int>()
-
-        required.addAll(fuel.inputs)
+    private fun reduce(required: ArrayList<CountAndType>): Long {
+        val extras = HashMap<String, Long>()
 
         while (!(required[0].type == "ORE" && required.size == 1)) {
             val nonOreRequirements = required.filter { it.type != "ORE" }.toHashSet()
             required.removeAll(nonOreRequirements)
-            val newRequirementsMap = HashMap<String, Int>()
+            val newRequirementsMap = HashMap<String, Long>()
 
             extras.keys.forEach { extraKey ->
                 nonOreRequirements.forEach { req ->
@@ -46,23 +42,28 @@ class DayFourteen(file: String) : Project {
                 }
             }
 
-            extras.filter { (_, value) -> value == 0 }.keys.forEach { extras.remove(it) }
+            extras.filter { (_, value) -> value == 0L }.keys.forEach { extras.remove(it) }
 
             nonOreRequirements.forEach {
-                println("Getting $it")
                 val reaction: Reaction = reactions[it.type]!!
                 var requiredCount = it.count
+                var runTimes = 0
                 while(requiredCount > 0) {
                     reaction.inputs.forEach { input ->
                         val newCount = (newRequirementsMap[input.type] ?: 0) + input.count
                         newRequirementsMap[input.type] = newCount
                     }
                     requiredCount -= reaction.output.count
+                    runTimes++
                     if (requiredCount < 0) {
                         extras[it.type] = (extras[it.type] ?: 0) + abs(requiredCount)
                     }
                 }
+                println(runTimes)
             }
+
+            println(newRequirementsMap)
+            println(extras)
 
             newRequirementsMap["ORE"] = (newRequirementsMap["ORE"] ?: 0) + if (required.size > 0) required[0].count else 0
             required.clear()
@@ -75,8 +76,35 @@ class DayFourteen(file: String) : Project {
         return required[0].count
     }
 
+    override fun part1(): Any {
+        val fuel = reactions["FUEL"]!!
+        val required = ArrayList<CountAndType>()
+        required.addAll(fuel.inputs)
+        return reduce(required)
+    }
+
     override fun part2(): Any {
-        return -1
+        val fuel = reactions["FUEL"]!!
+        val required = ArrayList<CountAndType>()
+
+        var ore = Long.MIN_VALUE
+        var fuelCount = 82892753
+
+        val requiredMap = HashMap<String, Long>()
+
+        fuel.inputs.forEach {
+            requiredMap[it.type] = it.count * fuelCount
+        }
+
+        requiredMap.forEach { (key, value) -> required.add(CountAndType(value, key)) }
+
+        if (!required.isEmpty()) {
+            ore = reduce(required)
+        }
+        fuelCount++
+        println(ore)
+
+        return fuelCount
     }
 
     companion object {
@@ -90,7 +118,7 @@ class DayFourteen(file: String) : Project {
             return "${inputs.joinToString(", ","","")} => $output"
         }
     }
-    class CountAndType(var count: Int, val type: String) {
+    class CountAndType(var count: Long, val type: String) {
         override fun toString(): String {
             return "$count $type"
         }
