a6a886ca14204c31486c0891855bed95aec0ad2b
jamie <jamie@checkin.tech>
2022-12-14 09:44:16 -0400

    #20: 2022, Day 14, Part 2

--- src/main/kotlin/2022/DayFourteen.kt
+++ src/main/kotlin/2022/DayFourteen.kt
@@ -17,7 +17,7 @@ class DayFourteen(file: String) : Project {
     503,4 -> 502,4 -> 502,9 -> 494,9
 */
 
-    val stuff = HashMap<Int, HashMap<Int, Thing>>()
+    var stuff = HashMap<Int, HashMap<Int, Thing>>()
     val lineCoords = mapFileLines(file) { it.split(" -> ").map { s -> Point(s.split(",")) } }
     var maxY = Int.MIN_VALUE
 
@@ -31,6 +31,10 @@ class DayFourteen(file: String) : Project {
     }
 
     init {
+        getStuff()
+    }
+
+    private fun getStuff() {
         val ys = HashSet<Int>()
         lineCoords.forEach { line ->
             var curr: Point? = null
@@ -108,7 +112,53 @@ class DayFourteen(file: String) : Project {
     }
 
     override fun part2(): Any {
-        return -1
+        stuff = HashMap()
+        getStuff()
+
+        var done = false
+        var sandCount = 0
+
+        while(!done) {
+            var sand = Point(500, 0)
+
+            while (!atRest(sand) && sand.y <= maxY) {
+                val row = stuff[sand.y + 1] ?: HashMap()
+                if (row[sand.x] == null) {
+                    sand = below(sand)
+                } else if(row[sand.x - 1] == null) {
+                    sand = left(sand)
+                } else if(row[sand.x + 1] == null) {
+                    sand = right(sand)
+                }
+            }
+            if (sand.y == maxY + 1) {
+                val row = stuff[sand.y] ?: HashMap()
+                row[sand.x] = Thing.SAND
+                stuff[sand.y] = row
+                sandCount++
+            } else if (atRest(sand)) {
+                val row = stuff[sand.y] ?: HashMap()
+                row[sand.x] = Thing.SAND
+                stuff[sand.y] = row
+                sandCount++
+            }
+
+            done = (stuff[0] ?: HashMap())[500] != null
+        }
+
+        printGrid()
+        return sandCount
+    }
+
+    fun printGrid() {
+        for (y in 0..10) {
+            for (x in 487..513) {
+                val item = (stuff[y] ?: HashMap())[x]
+                print(if(item == null) "." else if(item == Thing.SAND) "o" else "#")
+            }
+            println()
+        }
+        println("###########################")
     }
 }
 
