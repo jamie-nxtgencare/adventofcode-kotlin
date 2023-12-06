d0530179aacd41442356bed4db03fdcbdc0ee888
jamie <jamie@checkin.tech>
2023-12-06 14:24:26 -0400

    #29: Day 6 - Part 2

--- src/main/kotlin/2023/DaySix.kt
+++ src/main/kotlin/2023/DaySix.kt
@@ -3,16 +3,16 @@
 package `2023`
 
 import Project
-import java.lang.Integer.parseInt
+import java.lang.Long.parseLong
 
 class DaySix(file: String) : Project() {
     private val lines = getLines(file)
     private val timeArr = lines[0].replace(Regex("Time:\\s*"), "").split(" ")
     private val timeStr = timeArr.filter { it.isNotBlank() }
-    private val times = timeStr.map { parseInt(it) }
+    private val times = timeStr.map { parseLong(it) }
     private val distanceArr = lines[1].replace(Regex("Distance:\\s*"), "")
     private val distanceStr = distanceArr.split(" ").filter { it.isNotBlank() }
-    private val distances = distanceStr.map { parseInt(it) }
+    private val distances = distanceStr.map { parseLong(it) }
 
     override fun part1(): Any {
         return times.mapIndexed { i, time ->
@@ -31,7 +31,18 @@ class DaySix(file: String) : Project() {
     }
 
     override fun part2(): Any {
-        return -1
+        val time = parseLong(times.joinToString(""))
+        val distance = parseLong(distances.joinToString(""))
+        var waysToWin = 0
+
+        for (hold in 0 until time) {
+            val newDistance = hold * (time - hold)
+            if (newDistance > distance) {
+                waysToWin++
+            }
+        }
+
+        return waysToWin
     }
 
 }
--- src/test/kotlin/2023/Testing.kt
+++ src/test/kotlin/2023/Testing.kt
@@ -60,12 +60,12 @@ class Testing {
 
     @Test
     fun day6Sample() {
-        testSample(6, 288, -1)
+        testSample(6, 288, 71503)
     }
 
     @Test
     fun day6() {
-        test(6, -1, -1)
+        test(6, 1108800, -1)
     }
 
     @Test
