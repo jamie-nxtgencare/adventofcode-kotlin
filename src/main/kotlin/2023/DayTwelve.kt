5bf307725d1bd8de591799871a98ed1022aa4770
jamie <jamie@checkin.tech>
2023-12-20 14:29:17 -0400

    #34: Day 12, Part 2 -- works but too slow.

--- src/main/kotlin/2023/DayTwelve.kt
+++ src/main/kotlin/2023/DayTwelve.kt
@@ -7,58 +7,93 @@ import java.lang.Integer.parseInt
 
 class DayTwelve(file: String) : Project() {
     val records = getLines(file).map { it.split(" ") }
-    override fun part1(): Any {
+
+    inline fun <T, R,> Iterable<T>.filterThenMap(predicate: (T) -> Boolean, transform: (T) -> R): List<R> {
+        return filterThenMap(ArrayList(), predicate, transform)
+    }
+    inline fun <T, R, C: MutableCollection<in R>>
+            Iterable<T>.filterThenMap(collection: C, predicate: (T) -> Boolean,
+                                      transform: (T) -> R): C {
+        for(element in this) {
+            if (predicate(element)) {
+                collection.add(transform(element))
+            }
+        }
+        return collection
+    }
+
+    private fun doIt(folds: Int = 1): Any {
         return records.sumOf { record ->
-            val springList = record[0]
-            val damaged = record[1].split(",").map { parseInt(it) }
+            var springList = record[0]
+            var damagedStr = record[1]
 
-            val maxQs = springList.count { it == '?' }
-            val options = listOf("#", ".")
-            val combinations = getCombos(options, listOf(""), maxQs)
-
-            val guesses = combinations.map { combo ->
-                var thisCombo = springList
-                combo.forEach {
-                    thisCombo = thisCombo.replaceFirst('?', it)
-                }
-                thisCombo
+            if (folds != 1) {
+                springList = generateSequence { springList }.take(folds).joinToString("?")
+                damagedStr = generateSequence { damagedStr }.take(folds).joinToString(",")
             }
 
-            val guessDamageCounts = guesses.map { it.split(".").filter { it.isNotBlank() }.map { it.length } }
-            guessDamageCounts.filter { it == damaged }.size
+            val damaged = damagedStr.split(",").map { parseInt(it) }
+            val sieve = damaged.map { generateSequence { "#" }.take(it).toList().joinToString("") }
+
+            val options = mutableListOf(".$springList") // start with a dot so we can tell the code we're a new group
+            var i = 0
+            for (group in sieve) {
+                val matches = mutableListOf<String>()
+                val possibleOptions = options.filter { it.count { it == '?' || it == '#' } >= sieve.size - i  }
+
+                for (option in possibleOptions) {
+                    var o = option
+
+                    // We need to start with a . to ensure we're starting a new block to match this group
+                    if (o.startsWith("?")) {
+                        o = o.replaceFirst("?", ".")
+                    }
+
+                    if (!o.startsWith(".")) {
+                        continue
+                    }
+
+                    // trim garbage
+                    while (o.startsWith(".")) {
+                        o = o.removeRange(0 until 1)
+                    }
+
+                    val nextOptions = mutableListOf<String>()
+                    // until we have to make a chunk, fork into a chunk and skipping
+                    while (o.startsWith("?") || o.startsWith(".")) {
+                        if (o.startsWith("?")) {
+                            nextOptions.add(o.replaceFirst("?", "#"))
+                        }
+
+                        o = o.substring(1 until o.length)
+                    }
+                    nextOptions.add(o)
+
+                    if (o.startsWith(".")) {
+                        while (o.startsWith(".")) {
+                            o = o.removeRange(0 until 1)
+                        }
+                        nextOptions.add(o)
+                    }
+
+                    matches.addAll(nextOptions.filterThenMap({ it.length >= group.length && it.substring(group.indices).replace("?", "#").startsWith(group) }, { it.replaceRange(group.indices, "") }))
+                }
+                options.clear()
+                options.addAll(matches)
+                i++
+            }
+
+            val size = options.filter { !it.contains("#") }.size
+            println(size)
+            size
         }
     }
 
-    // ...
-    // .#.
-    // ..#
-    // #..
-    // ##.
-    // #.#
-    // ###
-    private fun getCombos(options: List<String>, prefixed: List<String>, maxQs: Int): List<String> {
-        mutableListOf<String>()
-
-        if (maxQs == 1) {
-            return prefixed.flatMap { prefix ->
-                options.map {
-                    prefix + it
-                }
-            }
-        }
-
-        val next = prefixed.flatMap { prefix ->
-            options.flatMap {
-                val next = mutableListOf<String>()
-                next.add(prefix + it)
-                next
-            }
-        }
-
-        return getCombos(options, next, maxQs -1)
+    override fun part1(): Any {
+        return doIt()
     }
 
     override fun part2(): Any {
-        return -1
+        return doIt(5)
     }
 }
