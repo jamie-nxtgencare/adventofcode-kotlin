78d13da5064b3168f45d6fa91188aa68049133bd
jamie <jamie@checkin.tech>
2023-12-21 10:59:03 -0400

    #37: Day 14, Part 1

--- src/main/kotlin/2023/DayFourteen.kt
+++ src/main/kotlin/2023/DayFourteen.kt
@@ -5,8 +5,38 @@ package `2023`
 import Project
 
 class DayFourteen(file: String) : Project() {
+    val grid = getLines(file).map { it.split("").filter { it.isNotBlank() }.toMutableList()}.toMutableList()
+
     override fun part1(): Any {
-        return -1
+        for (y in 1 until grid.size) {
+            for (x in grid[0].indices) {
+                if (grid[y][x] == "O") {
+                    var newY = y
+
+                    while(newY - 1 >= 0 && grid[newY - 1][x] == ".") {
+                        newY--
+                    }
+
+                    if (newY >= 0 && newY != y) {
+                        grid[y][x] = "."
+                        grid[newY][x] = "O"
+                    }
+                }
+            }
+        }
+
+        var load = 0
+        var distance = grid.size
+        for (y in grid.indices) {
+            for (x in grid[0].indices) {
+                if (grid[y][x] == "O") {
+                    load += distance
+                }
+            }
+            distance--
+        }
+
+        return load
     }
 
     override fun part2(): Any {
--- src/test/kotlin/2023/Testing.kt
+++ src/test/kotlin/2023/Testing.kt
@@ -143,7 +143,7 @@ class Testing {
 
     @Test
     fun day14Sample() {
-        testSample(14, -1, -1)
+        testSample(14, 136, -1)
     }
 
     @Test
